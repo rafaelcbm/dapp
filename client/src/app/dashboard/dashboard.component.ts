a5890c5f2d8c534cf93f77ce2e1da929380fa58e
Rafael Correia <rafaelcbm@gmail.com>
2017-09-27 00:14:26 -0300

    updates

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -3,20 +3,39 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  templateUrl: 'dashboard.component.html'
+    templateUrl: 'dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
+
 })
 export class DashboardComponent implements OnInit {
 
-  users: any[];
+    users: any[];
 
-  constructor(private http: HttpClient) { }
+    candidatos: any[] = [];
+    numeroCandidato = '';
+    nomeCandidato = '';
 
-  ngOnInit(): void {
+    constructor(private http: HttpClient) { }
 
-    this.http.get('/api/users/all').subscribe(
-      (resposta: any) => this.users = resposta.data
-    );
+    ngOnInit(): void {
 
-  }
+        this.http.get('/api/users/all').subscribe(
+            (resposta: any) => this.users = resposta.data
+        );
+    }
 
+    addCandidato() {
+        const candidato = { nome: this.nomeCandidato, numero: this.numeroCandidato };
+        console.log('cadidato - ', candidato)
+        this.candidatos.push(candidato);
+        console.log('this.candidatos - ', this.candidatos)
+    }
+
+    deleteCandidato(candidato) {
+        console.log('candidato removido>', candidato);
+    }
+
+    concluirVotacao(formValue) {
+        console.log('Votação concluida');
+    }
 }
--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
@@ -8,12 +9,13 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    DashboardRoutingModule,
-    ChartsModule
-  ],
-  declarations: [DashboardComponent]
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        DashboardRoutingModule,
+        ChartsModule,
+        FormsModule
+    ],
+    declarations: [DashboardComponent]
 })
 export class DashboardModule { }
